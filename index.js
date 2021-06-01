3f82a988b9833e45acba5398f56d1305e7b51a53
Jason Davis <jason.davis@kellogg.northwestern.edu>
2021-05-31 23:14:23 -0500

    edits for login and listings object

--- index.js
+++ index.js
@@ -2,6 +2,40 @@ firebase.auth().onAuthStateChanged(async function(user) {
   if (user) {
     // Signed in
     console.log('signed in')
+    
+    // write the user Object to the JavaScript console
+    //console.log(user)
+
+    // Build the markup for the sign-out button and set the HTML in the header
+    document.querySelector(`.sign-in-or-sign-out`).innerHTML = `
+      <button class="text-pink-500 underline sign-out">Sign Out</button>
+    `
+
+    // get a reference to the sign out button
+    let signOutButton = document.querySelector(`.sign-out`)
+
+    // handle the sign out button click
+    signOutButton.addEventListener(`click`, function(event) {
+      // sign out of firebase authentication
+      firebase.auth().signOut()
+
+      // redirect to the home page
+      document.location.href = `index.html`
+    })
+
+    // Build the URL for our posts API
+    let url = `/.netlify/functions/listings`
+
+    // Fetch the url, wait for a response, store the response in memory
+    let response = await fetch(url)
+
+    // Ask for the json-formatted data from the response, wait for the data, store it in memory
+    let json = await response.json()
+
+    // Write the json-formatted data to the console in Chrome
+    console.log(json)
+
+    
   } else {
     // Signed out
     console.log('signed out')
